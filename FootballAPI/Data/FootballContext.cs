0e51845b6bfb6d1dcd030661008b825d255adfeb
dangth92 <dangth92@gmail.com>
2020-09-12 04:33:38 +0700

    Add EF model sample, migration

---
+++ FootballAPI/Data/FootballContext.cs
@@ -0,0 +1,25 @@
+﻿using FootballAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FootballAPI.Data
+{
+    public class FootballContext : DbContext
+    {
+        public FootballContext(DbContextOptions<FootballContext> options): base(options)
+        {
+        }
+
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Fixture> Fixtures { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Course>().ToTable("Course");
+            modelBuilder.Entity<Enrollment>().ToTable("Enrollment");
+            modelBuilder.Entity<Student>().ToTable("Student");
+            modelBuilder.Entity<Fixture>().ToTable("Fixture");
+        }
+    }
+}
---
+++ FootballAPI/Models/Fixture.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FootballAPI.Models
+{
+    public class Fixture
+    {
+        public int ID { get; set; }
+        public int LocalTeamID { get; set; }
+        public int VisitorTeamID { get; set; }
+        public DateTime Time { get; set; }
+    }
+}
